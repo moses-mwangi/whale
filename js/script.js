fa6c080e769c2a297efa34572e5646acb877fa6b
moses <moses.mwangi.me@gmail.com>
2024-01-08 11:45:59 +0300

    commit

--- js/script.js
+++ js/script.js
@@ -2,13 +2,15 @@
 
 ///////////////////////////////////////
 // Modal window
+const navLogo = document.querySelector(".nav_logo");
 
 const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".btn--close-modal");
 const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
 
-const openModal = function () {
+const openModal = function (e) {
+  e.preventDefault();
   modal.classList.remove("hidden");
   overlay.classList.remove("hidden");
 };
@@ -18,8 +20,7 @@ const closeModal = function () {
   overlay.classList.add("hidden");
 };
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener("click", openModal);
+btnsOpenModal.forEach((btn) => btn.addEventListener("click", openModal));
 
 btnCloseModal.addEventListener("click", closeModal);
 overlay.addEventListener("click", closeModal);
@@ -29,3 +30,64 @@ document.addEventListener("keydown", function (e) {
     closeModal();
   }
 });
+
+/////////////////////////////////selecting element////////////////////
+
+console.log(document.documentElement);
+console.log(document.head);
+console.log(document.body);
+
+const header = document.querySelector(".header");
+const allSection = document.querySelectorAll(".section");
+console.log(allSection);
+
+const allb = document.getElementsByTagName("p");
+const ty = document.getElementsByClassName("btn");
+console.log(allb);
+
+//////////////////////////////// ceate and inserting element /////////////////////
+
+// const para = document.createElement("p");
+// para.classList.add("cookie-message");
+// const node = document.createTextNode("This is new.<p class=btn >got it</p>");
+// para.appendChild(node);
+// header.prepend(para);
+
+const message = document.createElement("div");
+message.classList.add("cookie-message");
+// message.textContent = "we use cookie for improved functionality and anlytical";
+message.innerHTML =
+  "we use cookie for improved functionality and anlytical <p class=btn >got it</p>";
+// header.prepend(message);
+header.append(message);
+// header.append(message.cloneNode(true));
+// header.before(message);
+// header.before(message);
+
+//////////////////////////////////////////// delete ///////////////////////////////////////
+document.querySelector(".btn").addEventListener("click", function () {
+  message.remove();
+});
+
+////////////////////////////////styling//////////////////////
+message.style.backgroundColor = "#345678";
+message.style.width = "120%";
+console.log(getComputedStyle(message).height);
+message.style.height =
+  Number.parseFloat(getComputedStyle(message).height, 10) + 30 + "px";
+
+document.documentElement.style.setProperty("--color-primary", "blue");
+
+const logo = document.querySelector(".nav__logo");
+logo.setAttribute("company", "bankist");
+// console.log(logo.getAttribute("designer"));
+
+logo.alt = "hahahahahah";
+console.log(logo.alt);
+console.log(logo.className);
+console.log(logo.src);
+console.log(logo.getAttribute("src"));
+
+////data atribute/////////////////////
+
+console.log(logo.dataset.versionNumber);
