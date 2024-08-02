830fb2491e8b016bdb738a4b57338375f365a2a0
moses mwangi <moses.mwangi.me@gmail.com>
2024-08-02 18:57:44 +0300

    commit

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const db =
 const DB =
   "mongodb+srv://mosesmwangime:9SPqAj4JOaXBxDrI@cluster0.sqjq7km.mongodb.net/natours?retryWrites=true&w=majority&appName=Cluster0";
 
-mongoose.connect(DB).then((con) => {
+mongoose.connect(db).then((con) => {
   console.log("Database connection successfull");
 });
 
